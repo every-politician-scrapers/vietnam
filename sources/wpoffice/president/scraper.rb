81502d97fb846f49835a0178544eb37e7e1adcd3
Tony Bowden <tony@tmtm.com>
2022-07-10 10:50:43 +0300

    compare enwiki Presidents

---
+++ sources/wpoffice/president/scraper.rb
@@ -0,0 +1,28 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Portrait'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[no _ img name start end].freeze
+    end
+
+    def raw_end
+      super.gsub('resignation', '')
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
