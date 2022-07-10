08ccf486a278f794e22906ccd0ff1aac7f1c1f31
Tony Bowden <tony@tmtm.com>
2022-07-10 10:00:54 +0300

    compare viwiki Bank Governor

---
+++ sources/wpoffice/bank-governor/scraper.rb
@@ -0,0 +1,41 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class String
+  def zeropad
+    rjust(2, '0')
+  end
+end
+
+# Vietnamese dates
+class Vietnamese < WikipediaDate
+  def to_s
+    date_str.to_s.gsub('đương nhiệm', '').gsub('nay', '').gsub('Từ', '').gsub(/tháng/i, '').gsub('năm', '').gsub(',', '').tidy.gsub(' ', '-').split('-').reverse.map(&:zeropad).join('-')
+  end
+end
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  def header_column
+    'Chức vụ'
+  end
+
+  class Officeholder < OfficeholderBase
+    def columns
+      %w[no name dates].freeze
+    end
+
+    def date_class
+      Vietnamese
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
