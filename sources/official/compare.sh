4cdc5a2422ff11bba8a3204ec7824ee6d90e8896
Tony Bowden <tony@tmtm.com>
2022-07-16 09:07:10 +0300

    remove unused official scraper

--- sources/official/compare.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd $(dirname $0)
-
-bundle exec ruby scraper.rb > scraped.csv
-wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid > wikidata.csv
-bundle exec ruby diff.rb | tee diff.csv
-
-cd ~-
